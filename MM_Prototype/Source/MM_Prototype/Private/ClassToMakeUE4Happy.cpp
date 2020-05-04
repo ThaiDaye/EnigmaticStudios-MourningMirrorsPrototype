ce9cdcbf7b467ee7b69ab1219946bd229686f4b0
Conor Vernon <cvernon.interactive@gmail.com>
2020-05-04 16:56:28 +0100

    Rebuilt project from VS

---
+++ MM_Prototype/Source/MM_Prototype/Private/ClassToMakeUE4Happy.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClassToMakeUE4Happy.h"
+
+ClassToMakeUE4Happy::ClassToMakeUE4Happy()
+{
+}
+
+ClassToMakeUE4Happy::~ClassToMakeUE4Happy()
+{
+}
